dc82760286db286aa2dbdd2014fa95c13380c3e3
HieuMIU <gtran@miu.edu>
2024-12-02 16:14:53 -0600

    -Update Model Item And ApplicationUser -Add ItemRepository and ApplicationUserRepository -Add ItemService and Application Service -Add ItemController and AccountController

--- ShareHubMIU.Domain/Entities/Item.cs
+++ ShareHubMIU.Domain/Entities/Item.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ShareHubMIU.Domain.Entities;
 
 namespace ShareHubMIU.Domain.Entities
 {
@@ -15,8 +18,12 @@ namespace ShareHubMIU.Domain.Entities
         public string Category { get; set; }
         public DateTime DateListed { get; set; }
         public bool IsAvailable { get; set; }
-        public int SellerId { get; set; }
-        public User Seller { get; set; }
+
+        [Required]
+        public string SellerId { get; set; }
+
+        [ForeignKey("SellerId")]
+        public ApplicationUser seller { get; set; }
     }
 
 }
---
+++ ShareHubMIU.Web/Controllers/AccountController.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ShareHubMIU.Application.Common.Interfaces;
+using ShareHubMIU.Application.Common.Utility;
+using ShareHubMIU.Domain.Entities;
+using ShareHubMIU.Web.ViewModel;
+
+namespace ShareHubMIU.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        private RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, 
+            SignInManager<ApplicationUser> signInManager, 
+            RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        
+        public IActionResult Login(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+
+            LoginVM loginVM = new()
+            {
+                ReturnUrl = returnUrl
+            };
+
+            return View(loginVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(loginVM.Email, loginVM.Password, loginVM.RememberMe, lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    var user = await _userManager.FindByEmailAsync(loginVM.Email);
+
+                    if( await _userManager.IsInRoleAsync(user, SD.Role_Admin))
+                    {
+                        return RedirectToAction("Index", "Dashboard");
+                    }
+                    else
+                    {
+                        if (string.IsNullOrEmpty(loginVM.ReturnUrl))
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+                        else
+                        {
+                            return LocalRedirect(loginVM.ReturnUrl);
+                        }
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid login attemp.");
+                }
+            }
+            return View(loginVM);
+        }
+
+        public IActionResult Register(string returnUrl = null)
+        {
+            RegisterVM registerVM = new RegisterVM()
+            {
+                RoleList = _roleManager.Roles.Select(u => new SelectListItem()
+                {
+                    Text = u.Name,
+                    Value = u.Name
+                })
+            };
+
+            return View(registerVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    Name = registerVM.Name,
+                    Email = registerVM.Email,
+                    PhoneNumber = registerVM.PhoneNumber,
+                    NormalizedEmail = registerVM.Email.ToUpper(),
+                    EmailConfirmed = true,
+                    UserName = registerVM.Email,
+                    CreatedAt = DateTime.Now
+                };
+
+                var result = await _userManager.CreateAsync(user, registerVM.Password);
+
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(registerVM.Role))
+                    {
+                        await _userManager.AddToRoleAsync(user, registerVM.Role);
+                    }
+                    else
+                    {
+                        await _userManager.AddToRoleAsync(user, SD.Role_Customer);
+                    }
+
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    if (string.IsNullOrEmpty(registerVM.ReturnUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        return LocalRedirect(registerVM.ReturnUrl);
+                    }
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            registerVM.RoleList = _roleManager.Roles.Select(u => new SelectListItem()
+            {
+                Text = u.Name,
+                Value = u.Name
+            });
+
+            return View(registerVM);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();    
+
+            return RedirectToAction("Index","Home");
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
